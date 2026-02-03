f958058916105412a383353f064db4087bbf7723
himanshu075-unique <himanshuchoudhary9365@gmail.com>
2026-02-03 22:43:33 +0530

    Sign up form

--- website/auth.py
+++ website/auth.py
@@ -5,7 +5,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login.html", text="Testing", user="Himanshu", boolean=True)
 
 @auth.route('/logout')
 def logout():
